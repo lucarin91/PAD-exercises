b923d176ae7e84c006dde653e3d9626ee3b7722d
lucarin91 <lucarin91@gmail.com>
2015-12-20 21:34:01 +0100

    bo

---
+++ ConsistenHashing/conshash/ConsistentHash.java
@@ -0,0 +1,7 @@
+package conshash;
+
+class TestConsistentHashing{
+  public static void main(String[] args){
+    System.out.println("Consistent Hashing!");
+  }
+}
---
+++ ConsistenHashing/conshash/HashTable.java
@@ -0,0 +1,16 @@
+
+
+
+class ConsistentHash {
+
+  private HashTable hash;
+
+  public ConsistentHash(){
+      hash = new HashTable();
+  }
+
+  public void addItem(){
+
+  }
+
+}
